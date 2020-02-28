fa0d784e5ac359e35e72340446812132960f5e52
Emily Gomez <emgomez@unomaha.edu>
2020-02-27 23:25:50 -0600

    Finished up the Inventory Insert page and touched up some other parts

--- webproject-inventory-gomez/src/InsertGomez.java
+++ webproject-inventory-gomez/src/InsertGomez.java
@@ -15,66 +15,92 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/InsertGomez")
 public class InsertGomez extends HttpServlet {
-   private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-   public InsertGomez() {
-      super();
-   }
+	public InsertGomez() {
+		super();
+	}
 
-   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      String userName = request.getParameter("userName");
-      String item = request.getParameter("item");
-      String quantity = request.getParameter("quantity");
-      String location = request.getParameter("location");
-      String value = request.getParameter("value");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userName = request.getParameter("userName");
+		String item = request.getParameter("item");
+		String quantity = request.getParameter("quantity");
+		String location = request.getParameter("location");
+		String value = request.getParameter("value");
 
 
-      Connection connection = null;
-      String insertSql = " INSERT INTO projectTable (id, USERNAME, ITEM, QUANTITY, LOCATION, VALUE) values (default, ?, ?, ?, ?, ?)";
+		Connection connection = null;
+		String insertSql = " INSERT INTO projectTable (id, USERNAME, ITEM, QUANTITY, LOCATION, VALUE) values (default, ?, ?, ?, ?, ?)";
 
-      try {
-         DBConnectionGomez.getDBConnection(getServletContext());
-         connection = DBConnectionGomez.connection;
-         PreparedStatement preparedStmt = connection.prepareStatement(insertSql);
-         preparedStmt.setString(1, userName);
-         preparedStmt.setString(2, item);
-         preparedStmt.setString(3, quantity);
-         preparedStmt.setString(4, location);
-         preparedStmt.setString(5, value);
+		try {
+			DBConnectionGomez.getDBConnection(getServletContext());
+			connection = DBConnectionGomez.connection;
+			PreparedStatement preparedStmt = connection.prepareStatement(insertSql);
+			preparedStmt.setString(1, userName);
+			preparedStmt.setString(2, item);
+			preparedStmt.setString(3, quantity);
+			preparedStmt.setString(4, location);
+			preparedStmt.setString(5, value);
 
-         preparedStmt.execute();
-         connection.close();
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
+			preparedStmt.execute();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-      // Set response content type
-      response.setContentType("text/html");
-      PrintWriter out = response.getWriter();
-      String title = "Insert Data to DB table";
-      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
-      out.println(docType + //
-            "<html>\n" + //
-            "<head><title>" + title + "</title></head>\n" + //
-            "<body bgcolor=\"#f0f0f0\">\n" + //
-            "<h2 align=\"center\">" + title + "</h2>\n" + //
-            "<ul>\n" + //
+		// Set response content type
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String title = "Insert Data to DB table";
+		String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+		out.println(docType + //
+				"<html>\n" + //
+				"<head><title>" + title + "</title></head>\n" + //
+				"<body><header>\\n" + //
+				"<h1 align=\"center\">Inserted Data</h1>\n" + //
+				"<style> header { background-color:5e2bff;" + //
+					"color:white;" + //
+					"text-align:center;"+ //
+					"padding:5px;}" + //	 
+				"nav {line-height:30px;"+ //
+					"background-color:#eeeeee;"+ //
+					"height:300px;"+ //
+					"width:100px;"+ //
+					"float:left;"+ //
+					"padding:5px;}"+ //	      
+				"section {width:350px;"+ //
+					"float:left;"+ //
+					"padding:10px;}"+ //
+				"footer { background-color:5e2bff;"+ //
+					"color:white;"+ //
+					"clear:both;"+ //
+					"text-align:center;"+ //
+					"padding:5px;}"+ //	 	 
 
-            "  <li><b>User Name</b>: " + userName + "\n" + //
-            "  <li><b>Item</b>: " + item + "\n" + //
-            "  <li><b>Quantity</b>: " + quantity + "\n" + //
-            "  <li><b>Location</b>: " + location + "\n" + //
-            "  <li><b>Value</b>: " + value + "\n" + //
+	            "</style></head>\n" + //
 
-            "</ul>\n");
+	            "</header>" + //
 
-      out.println("<a href=/webproject-inventory-gomez/search_gomez.html>Search Data</a> <br>");
-      out.println("<a href=/webproject-inventory-gomez/insert_gomez.html>Insert Data</a> <br>");
-      out.println("</body></html>");
-   }
+	            "<nav><a href=search_gomez.html>Search User</a> <br>" + //
+	            "<a href=insert_gomez.html>Insert Item</a> <br></nav>" + //
+	            "<section>" + //
+	            
+	            
+	            "For user <b>" + userName + "</b> the following has been added" + //
+	            "<ul>\n" + //
 
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      doGet(request, response);
-   }
+		            "  <li><b>Item</b>: " + item + "\n" + //
+		            "  <li><b>Quantity</b>: " + quantity + "\n" + //
+		            "  <li><b>Location</b>: " + location + "\n" + //
+		            "  <li><b>Value</b>: " + value + "\n" + //
+
+				"</ul>\n");
+
+		out.println("</table></section><footer>Copyright @Gomez</footer></body></html>");
+		}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }
--- webproject-inventory-gomez/src/SearchGomez.java
+++ webproject-inventory-gomez/src/SearchGomez.java
@@ -13,126 +13,123 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/SearchGomez")
 public class SearchGomez extends HttpServlet {
-   private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-   public SearchGomez() {
-      super();
-   }
+	public SearchGomez() {
+		super();
+	}
 
-   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      String keyword = request.getParameter("keyword");
-      search(keyword, response);
-   }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String keyword = request.getParameter("keyword");
+		search(keyword, response);
+	}
 
-   void search(String keyword, HttpServletResponse response) throws IOException {
-      response.setContentType("text/html");
-      PrintWriter out = response.getWriter();
-      String title = "Inventory List";
-      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + //
-            "transitional//en\">\n"; //
-      out.println(docType + //
-            "<html>\n" + //
-            "<body><header>\n" + //
-            "<head><title>" + title + "</title>" + //
-            "<style> header { background-color:5e2bff;" + //
-                "color:white;" + //
-                "text-align:center;"+ //
-                "padding:5px;}" + //	 
-            "nav {line-height:30px;"+ //
-                "background-color:#eeeeee;"+ //
-                "height:300px;"+ //
-                "width:100px;"+ //
-                "float:left;"+ //
-                "padding:5px;}"+ //	      
-            "section {width:350px;"+ //
-                "float:left;"+ //
-                "padding:10px;}"+ //
-            "footer { background-color:5e2bff;"+ //
-                "color:white;"+ //
-                "clear:both;"+ //
-                "text-align:center;"+ //
-                "padding:5px;}"+ //	 	 
-  
+	void search(String keyword, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String title = "Inventory List";
+		String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + //
+				"transitional//en\">\n"; //
+		out.println(docType + //
+				"<html>\n" + //
+				"<body><header>\n" + //
+				"<head><title>" + title + "</title>" + //
+				"<style> header { background-color:5e2bff;" + //
+				"color:white;" + //
+				"text-align:center;"+ //
+				"padding:5px;}" + //	 
+				"nav {line-height:30px;"+ //
+				"background-color:#eeeeee;"+ //
+				"height:300px;"+ //
+				"width:100px;"+ //
+				"float:left;"+ //
+				"padding:5px;}"+ //	      
+				"section {width:350px;"+ //
+				"float:left;"+ //
+				"padding:10px;}"+ //
+				"footer { background-color:5e2bff;"+ //
+				"color:white;"+ //
+				"clear:both;"+ //
+				"text-align:center;"+ //
+				"padding:5px;}"+ //	 	 
+
             "</style></head>\n" + //
 
             "<h1 align=\"center\">" + title + "</h1>\n</header>" + //
-            
+
             "<nav><a href=search_gomez.html>Search User</a> <br>" + //
-      		"<a href=insert_gomez.html>Insert Item</a> <br></nav>" + //
-      		"<section>");
+            "<a href=insert_gomez.html>Insert Item</a> <br></nav>" + //
+				"<section>");
 
-      Connection connection = null;
-      PreparedStatement preparedStatement = null;
-      try {
-         DBConnectionGomez.getDBConnection(getServletContext());
-         connection = DBConnectionGomez.connection;
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		try {
+			DBConnectionGomez.getDBConnection(getServletContext());
+			connection = DBConnectionGomez.connection;
 
-         if (keyword.isEmpty()) {
-            String selectSQL = "SELECT * FROM projectTable";
-            preparedStatement = connection.prepareStatement(selectSQL);
-         } else {
-            String selectSQL = "SELECT * FROM projectTable WHERE USERNAME LIKE ?";
-            String theUserName = keyword + "%";
-            preparedStatement = connection.prepareStatement(selectSQL);
-            preparedStatement.setString(1, theUserName);
-         }
-         ResultSet rs = preparedStatement.executeQuery();
-         
-         out.println("User Account <b>" + keyword + "</b><br>");
-         out.println("<table>");
-         out.println("<tr>");
-         out.println("<td><b>Item</b></td>");
-         out.println("<td><b>Quantity</b></td>");
-         out.println("<td><b>Location</b></td>");
-         out.println("<td><b>Value</b></td>");
-         out.println("</tr>");
+			if (keyword.isEmpty()) {
+				String selectSQL = "SELECT * FROM projectTable";
+				preparedStatement = connection.prepareStatement(selectSQL);
+			} else {
+				String selectSQL = "SELECT * FROM projectTable WHERE USERNAME LIKE ?";
+				String theUserName = keyword + "%";
+				preparedStatement = connection.prepareStatement(selectSQL);
+				preparedStatement.setString(1, theUserName);
+			}
+			ResultSet rs = preparedStatement.executeQuery();
+
+			out.println("User Account <b>" + keyword + "</b><br>");
+			out.println("<table>");
+			out.println("<tr>");
+			out.println("<td><b>Item</b></td>");
+			out.println("<td><b>Quantity</b></td>");
+			out.println("<td><b>Location</b></td>");
+			out.println("<td><b>Value</b></td>");
+			out.println("</tr>");
 
 
-         while (rs.next()) {
-             int id = rs.getInt("id");
-             String userName = rs.getString("username").trim();
-             String item = rs.getString("item").trim();
-             String quantity = rs.getString("quantity").trim();
-             String location = rs.getString("location").trim();
-             String value = rs.getString("value").trim();
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String userName = rs.getString("username").trim();
+				String item = rs.getString("item").trim();
+				String quantity = rs.getString("quantity").trim();
+				String location = rs.getString("location").trim();
+				String value = rs.getString("value").trim();
 
-             if (keyword.isEmpty() || userName.contains(keyword)) {
-//                out.println("ID: " + id + ", ");
-//                out.println("User: " + userName + ", ");
-                out.println("<tr><td>" + item + "</td>");
-                out.println("<td>" + quantity + "</td>");
-                out.println("<td>" + location + "</td>");
-                out.println("<td>" + value + "</td></tr>");
-             }
-          }
-//         out.println("<a href=/webproject-inventory-gomez/search_gomez.html>Search Data</a> <br>");
-//         out.println("<a href=/webproject-inventory-gomez/insert_gomez.html>Insert Data</a> <br>");
-         out.println("</table></section><footer>Copyright @Gomez</footer></body></html>");
-         out.println("</body></html>");
-         rs.close();
-         preparedStatement.close();
-         connection.close();
-      } catch (SQLException se) {
-         se.printStackTrace();
-      } catch (Exception e) {
-         e.printStackTrace();
-      } finally {
-         try {
-            if (preparedStatement != null)
-               preparedStatement.close();
-         } catch (SQLException se2) {
-         }
-         try {
-            if (connection != null)
-               connection.close();
-         } catch (SQLException se) {
-            se.printStackTrace();
-         }
-      }
-   }
+				if (keyword.isEmpty() || userName.contains(keyword)) {
+					//                out.println("ID: " + id + ", ");
+					//                out.println("User: " + userName + ", ");
+					out.println("<tr><td>" + item + "</td>");
+					out.println("<td>" + quantity + "</td>");
+					out.println("<td>" + location + "</td>");
+					out.println("<td>" + value + "</td></tr>");
+				}
+			}
+			out.println("</table></section><footer>Copyright @Gomez</footer></body></html>");
+			rs.close();
+			preparedStatement.close();
+			connection.close();
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} catch (SQLException se2) {
+			}
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+	}
 
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      doGet(request, response);
-   }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }
